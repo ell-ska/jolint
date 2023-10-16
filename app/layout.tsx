b5893d4966ca1dbd560fb2935fdc6c2bfd01b958
ell-ska <ella@skagerstrom.se>
2023-10-16 11:16:59 +0200

    add colors, fonts, and bg gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
-import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Space_Grotesk, Kumbh_Sans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { cn } from '@/utils/classnames'
+import './globals.css'
+
+const kumbhSans = Kumbh_Sans({ subsets: ['latin'], variable: '--kumbh-sans' })
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--space-grotesk',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +22,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          spaceGrotesk.variable,
+          kumbhSans.variable,
+          'font-body text-neutral-900',
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,31 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    colors: {
+      neutral: {
+        100: '#FFFFFF',
+        200: '#F1F5F9',
+        300: '#E2E8F0',
+        400: '#CBD5E1',
+        600: '#94A3B8',
+        700: '#94A3B8',
+        900: '#0F172A'
+      },
+      blue: {
+        dull: '#2C3386',
+        bright: '#0015CE',
+        light: '#91BBE7'
+      },
+      green: '#7ACD6D',
+      orange: '#F4770A',
+      red: '#D3383B'
+    },
+    fontFamily: {
+      body: 'var(--kumbh-sans)',
+      heading: 'var(--space-grotesk)'
+    }
+  },
   plugins: [],
 }
 export default config
