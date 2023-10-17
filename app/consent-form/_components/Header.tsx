1127edf5082385b014ad55e6d79b8662734adc6c
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-10-17 15:03:57 +0200

    made component inside header tag

--- app/consent-form/_components/Header.tsx
+++ app/consent-form/_components/Header.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-
 import Logo from '@/public/Jolint-logo.svg'
 import Image from 'next/image'
 
 export const Header = () => {
   return (
-    <>
+    <header>
       <div className='m-5'>
         <Image className='bg-white' src={Logo} alt='Logo' />
       </div>
       <hr className='text-neutral-400' />
-    </>
+    </header>
   )
 }
