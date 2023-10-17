e4068205cd36b719a459f291aa575ad02a899616
idamakela <idamakela98@gmail.com>
2023-10-17 14:32:28 +0200

    properly renamed the component type <3

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,11 +1,11 @@
 import { cn } from '@/utils/classnames'
 
-type CardType = {
+type CardProps = {
   classname: string
   children: React.ReactNode
 }
 
-const Card = ({ classname, children }: BaseDivType) => {
+const Card = ({ classname, children }: CardProps) => {
   return (
     <div className={cn('rounded-[2rem] bg-neutral-100 p-8', classname)}>
       {children}
