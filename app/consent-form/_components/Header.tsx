09e84620688c2a5852c9bdc76ca6658b63f1d7e2
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-10-18 12:14:26 +0200

    change border-color

--- app/consent-form/_components/Header.tsx
+++ app/consent-form/_components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export const Header = () => {
   return (
-    <header className='border-b border-neutral-600 p-5 '>
+    <header className='border-b border-neutral-400 p-5 '>
       <Image src={Logo} alt='Logo' />
     </header>
   )
