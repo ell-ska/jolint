727f3f2ffeb17769e55bbbfceecda76598765ab9
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-10-19 13:43:14 +0200

    merge fix/j41-create-header-consent-form to develop

--- app/consent-form/layout.tsx
+++ app/consent-form/layout.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import Header from '@/app/consent-form/_components/Header'
 import Footer from '@/app/consent-form/_components/Footer'
-=======
-import Footer from '@/app/consent-form/_components/Footer'
-import Header from '@/app/consent-form/_components/Header'
->>>>>>> fix/j41-create-header-consent-form
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
