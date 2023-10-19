65e73cd68e5dad1f64ca58d48aee5986148f7f43
idamakela <idamakela98@gmail.com>
2023-10-19 15:26:01 +0200

    finished SideBar and implemented it in dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,28 @@
+import { UrlsType } from '@/utils/types'
+import { Gauge, Settings } from 'lucide-react'
+
+import SideBar from './_components/SideBar'
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>
+  const urls: UrlsType = [
+    {
+      name: 'dashboard',
+      href: '/dashboard',
+      icon: <Gauge />,
+    },
+    {
+      name: 'settings',
+      href: '/dashboard/settings',
+      icon: <Settings />,
+    },
+  ]
+
+  return (
+    <main className='flex min-h-screen'>
+      <SideBar urls={urls} />
+      {children}
+    </main>
+  )
 }
 
 export default Layout
