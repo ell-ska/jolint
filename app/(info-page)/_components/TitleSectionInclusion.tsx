47c63b58d2177b1d060ff8255cfeb137379e67a2
robonexx <robertwagar@gmail.com>
2023-10-20 07:51:50 +0200

    title section for inclusion page

---
+++ app/(info-page)/_components/TitleSectionInclusion.tsx
@@ -0,0 +1,17 @@
+const TitleSectionInclusion = () => {
+  return (
+    <section className='flex flex-col gap-8 pb-8'>
+      <h1 className='w-2/3 space-x-8 font-heading text-5xl font-bold text-neutral-900'>
+        Why inclusion is important
+      </h1>
+      <p className='w-3/4 font-body text-2xl text-[#121535]'>
+        Inclusion and diversity play crucial roles in helping a workplace
+        thrive. By fostering an inclusive environment, several positive outcomes
+        can be achieved, including happiness, creativity, innovation, and
+        success. Let&apos;s explore each of these aspects.
+      </p>
+    </section>
+  )
+}
+
+export default TitleSectionInclusion
--- app/(info-page)/inclusion/page.tsx
+++ app/(info-page)/inclusion/page.tsx
@@ -1,5 +1,12 @@
+import TitleSectionInclusion from '@/app/(info-page)/_components/TitleSectionInclusion'
+
 const Inclusion = () => {
-  return <div>inclusion</div>
+  return (
+    <div className='min-h-screen relative px-20 py-28'>
+      <div className='max-h-screen colorfull-gradient-inclusion absolute left-0 top-0 -z-50 h-screen w-full'></div>
+      <TitleSectionInclusion />
+    </div>
+  )
 }
 
 export default Inclusion
