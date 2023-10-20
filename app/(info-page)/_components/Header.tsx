727d0c73b0a606edd36adf48056c323e4aca6db8
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-10-20 11:46:54 +0200

    remove English link and contact link

--- app/(info-page)/_components/Header.tsx
+++ app/(info-page)/_components/Header.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Globe, ChevronDown } from 'lucide-react'
 
 import Button from '@/components/Button'
 import Logo from '@/public/Jolint-logo.svg'
@@ -25,14 +24,9 @@ const Header = () => {
           <Link className='hover:underline' href='/about'>
             About us
           </Link>
-          <Link className='hover:underline' href='#'>
-            Contact us
+          <Link href='/consent-form'>
+            <Button variant='secondary'>Consent form</Button>
           </Link>
-          <p className='flex cursor-pointer items-center gap-2 hover:underline'>
-            <Globe className='h-4 w-4' /> English
-            <ChevronDown className='cursor-pointer' />
-          </p>
-          <Button variant='secondary'>Sign in</Button>
         </div>
       </div>
     </header>
