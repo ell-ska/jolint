13521fbf78e82e1ce2437ef1e7f21991db6094e1
Ida Mäkelä <idamakela98@gmail.com>
2023-10-21 14:19:03 +0200

    work in progress: feature/j68

--- app/(info-page)/about/page.tsx
+++ app/(info-page)/about/page.tsx
@@ -1,5 +1,20 @@
 const About = () => {
-  return <div>about</div>
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        At Jolint, we are driven by a passion for improving workplaces and
+        creating a better world for everyone. We make sure to keep ethics and
+        privacy at the forefront when gathering data. Throughout our journey,
+        we&apos;ve stayed committed to these principles, ensuring that
+        individuals&apos; information is handled with care and respect. Discover
+        the journey of Jolint, and join us in our quest to make the world a
+        better place, one company at a time. With our innovative approach,
+        cutting-edge technologies, and a team of experts, we are committed to
+        creating a more inclusive and equitable work culture for all.
+      </p>
+    </div>
+  )
 }
 
 export default About
