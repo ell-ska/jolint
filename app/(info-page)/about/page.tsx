fabb4005669e7f6ca9a9a0545645ec3de0be9a0d
idamakela <idamakela98@gmail.com>
2023-10-23 16:34:01 +0200

    removed comment

--- app/(info-page)/about/page.tsx
+++ app/(info-page)/about/page.tsx
@@ -1,5 +1,4 @@
 const About = () => {
-  // move over main padding to layout file?
   return (
     <main className='min-h-screen bg-gradient-to-b from-[#EEF3F8] px-20 pt-28'>
       <h1 className='pt-28 font-heading text-3xl font-bold capitalize md:text-5xl'>
