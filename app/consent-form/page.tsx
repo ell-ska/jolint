8d900a53ae915d8e942bf560c72858908e4eb3f4
ell-ska <ella@skagerstrom.se>
2023-10-23 13:56:37 +0200

    add form content

--- app/consent-form/page.tsx
+++ app/consent-form/page.tsx
@@ -1,4 +1,10 @@
+'use client'
+
+import { useForm } from '@/hooks/useForm'
+
 const ConsentForm = () => {
+  // const {} = useForm()
+
   return <main className='min-h-screen px-20 pt-36'></main>
 }
 
