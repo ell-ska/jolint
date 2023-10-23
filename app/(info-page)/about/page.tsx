4138a69b742f857811114640a302cbdeebd863ab
idamakela <idamakela98@gmail.com>
2023-10-23 16:32:33 +0200

    linear gradient and title done for feature/j68

--- app/(info-page)/about/page.tsx
+++ app/(info-page)/about/page.tsx
@@ -1,19 +1,11 @@
 const About = () => {
+  // move over main padding to layout file?
   return (
-    <div>
-      <h1>About us</h1>
-      <p>
-        At Jolint, we are driven by a passion for improving workplaces and
-        creating a better world for everyone. We make sure to keep ethics and
-        privacy at the forefront when gathering data. Throughout our journey,
-        we&apos;ve stayed committed to these principles, ensuring that
-        individuals&apos; information is handled with care and respect. Discover
-        the journey of Jolint, and join us in our quest to make the world a
-        better place, one company at a time. With our innovative approach,
-        cutting-edge technologies, and a team of experts, we are committed to
-        creating a more inclusive and equitable work culture for all.
-      </p>
-    </div>
+    <main className='min-h-screen bg-gradient-to-b from-[#EEF3F8] px-20 pt-28'>
+      <h1 className='pt-28 font-heading text-3xl font-bold capitalize md:text-5xl'>
+        about us
+      </h1>
+    </main>
   )
 }
 
