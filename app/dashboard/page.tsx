e1666827b02f16c7aa43fcee707eb468c600e7c4
ell-ska <ella@skagerstrom.se>
2023-10-23 10:21:56 +0200

    forgot to remove the rendered component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,5 @@
-'use client'
-import Dropdown from './_components/Dropdown'
-
 const Dashboard = () => {
-  return (
-    <div className='p-20'>
-      <Dropdown
-        onSelect={() => {}}
-        options={['hej', 'test', 'nått långt']}
-        selected={'hej'}
-      />
-    </div>
-  )
+  return <div>dashboard</div>
 }
 
 export default Dashboard
