30e459a270f14d086564a1b735a64ce47114a15a
idaakermark <ida.akermark@edu.cmeducations.se>
2023-10-24 12:00:42 +0200

    FAQ title section and backgroud gradient

--- app/(info-page)/faq/page.tsx
+++ app/(info-page)/faq/page.tsx
@@ -1,5 +1,12 @@
 const Faq = () => {
-  return <div>faq</div>
+  return (
+    <div className='orange-gradient min-h-screen px-6 pt-28 md:px-20'>
+      <h1 className='font-heading text-3xl font-bold md:text-5xl'>FAQ</h1>
+      <p className='pt-6 text-lg md:pt-6 md:text-2xl'>
+        Learn more about Jolint and our work method
+      </p>
+    </div>
+  )
 }
 
 export default Faq
