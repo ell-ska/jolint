ae85a66389bf61df660f9e6a970438200c8e27fe
idaakermark <ida.akermark@edu.cmeducations.se>
2023-10-24 13:25:29 +0200

    Fixing the responsive padding top

--- app/(info-page)/faq/page.tsx
+++ app/(info-page)/faq/page.tsx
@@ -1,6 +1,6 @@
 const Faq = () => {
   return (
-    <section className='min-h-[calc(100vh-5rem)] pt-28 md:min-h-[calc(100vh-7rem)]'>
+    <section className='min-h-[calc(100vh-5rem)] md:pt-28 pt-20 md:min-h-[calc(100vh-7rem)]'>
       <div className='orange-gradient absolute inset-0 -z-10' />
       <h1 className='font-heading text-3xl font-bold md:text-5xl'>FAQ</h1>
       <p className='pt-6 text-lg md:pt-6 md:text-2xl'>
