87fc7317f2fc59210e7108be201a5ae663331a27
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-10-24 13:49:36 +0200

    change closemenu to usePathname

--- app/(info-page)/_components/Header.tsx
+++ app/(info-page)/_components/Header.tsx
@@ -3,7 +3,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { X, Menu } from 'lucide-react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation'
 
 import { cn } from '@/utils/classnames'
 import Button from '@/components/Button'
@@ -11,13 +12,14 @@ import Logo from '@/public/Jolint-logo.svg'
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setIsOpen(false)
+  }, [pathname])
 
   const handleClick = () => {
     setIsOpen(!isOpen)
-  }
-
-  const closeMenu = () => {
-    setIsOpen(false)
   }
 
   return (
@@ -33,7 +35,7 @@ const Header = () => {
           isOpen && 'absolute inset-0 justify-center bg-neutral-100',
         )}
       >
-        {isOpen ? null : (
+        {!isOpen && (
           <Link href='/'>
             <Image className='cursor-pointer' src={Logo} alt='Logo' />
           </Link>
@@ -45,38 +47,26 @@ const Header = () => {
             isOpen && 'flex flex-col justify-center',
           )}
         >
-          <Link
-            className='hover:underline'
-            href='/#how-it-works'
-            onClick={closeMenu}
-          >
+          <Link className='hover:underline' href='/#how-it-works'>
             How it works
           </Link>
-          <Link
-            className='hover:underline'
-            href='/inclusion'
-            onClick={closeMenu}
-          >
+          <Link className='hover:underline' href='/inclusion'>
             Inclusion
           </Link>
-          <Link className='hover:underline' href='/faq' onClick={closeMenu}>
+          <Link className='hover:underline' href='/faq'>
             FAQ
           </Link>
-          <Link className='hover:underline' href='/about' onClick={closeMenu}>
+          <Link className='hover:underline' href='/about'>
             About us
           </Link>
           <Link href='/consent-form'>
-            <Button
-              className={cn(isOpen && 'mt-8')}
-              variant='secondary'
-              onClick={closeMenu}
-            >
+            <Button className={cn(isOpen && 'mt-8')} variant='secondary'>
               Consent form
             </Button>
           </Link>
         </div>
         <button
-          onClick={() => handleClick()}
+          onClick={handleClick}
           className='absolute right-8 top-9 z-20 p-1 md:hidden'
         >
           {!isOpen ? <Menu /> : <X />}
