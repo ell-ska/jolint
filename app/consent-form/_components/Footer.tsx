7a1a7776dcff3f7d892e82331be22b6826264157
idaakermark <ida.akermark@edu.cmeducations.se>
2023-10-24 15:15:31 +0200

    Made changes to pb and using gap instead of space-y

--- app/consent-form/_components/Footer.tsx
+++ app/consent-form/_components/Footer.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='bg-blue-dull px-8 text-center text-base text-neutral-100 md:space-y-8 md:px-32 md:pb-8 md:pt-20 md:text-left md:text-xl'>
+    <footer className='bg-blue-dull px-8 pb-4 text-center text-base text-neutral-100 md:space-y-8 md:px-32 md:pb-8 md:pt-20 md:text-left md:text-xl'>
       <div className='flex flex-col items-center pt-8 md:flex-row md:justify-between'>
         <div className='space-y-2 md:space-y-4'>
           <p>Jolint Network Analytics AB</p>
           <p>Org. Nr.559324-1770</p>
           <p>Gothenburg, Sweden</p>
         </div>
-        <div className='flex flex-col items-center space-y-2 py-12 text-xl md:items-start md:space-y-4 md:py-0'>
+        <div className='flex flex-col items-center gap-2 py-12 text-xl md:items-start md:gap-4 md:py-0'>
           <Link
             href='https://www.linkedin.com/company/jolint-network-analytics-ab/'
             className='flex gap-4'
@@ -22,7 +22,7 @@ const Footer = () => {
           </Link>
         </div>
       </div>
-      <p className='pb-4 text-center'>
+      <p className='text-center'>
         Copyright © 2023 Jolint | Powered by Jolint
       </p>
     </footer>
