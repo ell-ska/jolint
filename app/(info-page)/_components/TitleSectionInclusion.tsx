f32647f884502c8a612a68af019fab8cd9a2e0fd
robonexx <robertwagar@gmail.com>
2023-10-25 16:38:28 +0200

    ...

--- app/(info-page)/_components/TitleSectionInclusion.tsx
+++ app/(info-page)/_components/TitleSectionInclusion.tsx
@@ -4,7 +4,7 @@ const TitleSectionInclusion = () => {
       <h1 className='w-2/3 space-x-8 font-heading text-5xl font-bold text-neutral-900'>
         Why inclusion is important
       </h1>
-      <p className='w-3/4 font-body text-2xl text-[#121535]'>
+      <p className='w-3/4 font-body text-2xl text-neutral-900'>
         Inclusion and diversity play crucial roles in helping a workplace
         thrive. By fostering an inclusive environment, several positive outcomes
         can be achieved, including happiness, creativity, innovation, and
