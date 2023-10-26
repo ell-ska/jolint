ea9d2834b0c802714725374574d04f78a9463a76
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-10-26 16:19:06 +0200

    added less margin for mobile screen

--- app/consent-form/_components/form/FormContent.tsx
+++ app/consent-form/_components/form/FormContent.tsx
@@ -4,7 +4,7 @@ const FormContent = ({ content, title }: FormChapterProps) => {
   return (
     <div className='mb-12 mt-8 max-w-3xl'>
       <h2 className='text-xl font-bold md:text-2xl'>{title}</h2>
-      <div className='mt-6 space-y-8'>
+      <div className='mt-4 space-y-8 md:mt-6'>
         {content.map((text) => (
           <p className='leading-8' key={text}>
             {text}
