132a6c0eb021da0d38839f11a50024222bb7878a
idamakela <idamakela98@gmail.com>
2023-10-26 16:14:43 +0200

    updated playground for testing

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,21 +1,34 @@
 import Accordion from '@/components/Accordion'
+import GeneralGridLayout from '../dashboard/_components/GeneralGridLayout'
+import Sidebar from '@/app/dashboard/_components/Sidebar'
+import { Gauge, Settings } from 'lucide-react'
+import { UrlsType } from '@/utils/types'
+
+const urls: UrlsType = [
+  {
+    name: 'dashboard',
+    href: '/dashboard',
+    icon: <Gauge />,
+  },
+  {
+    name: 'settings',
+    href: '/dashboard/settings',
+    icon: <Settings />,
+  },
+]
 
 const Playground = () => {
   return (
-    <div className='p-8'>
-      <Accordion
-        className='max-w-xs space-y-4 rounded-2xl bg-neutral-200 p-4'
-        trigger={<h3 className='text-xl font-bold'>hej hej</h3>}
-        content={
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sunt
-            deleniti nobis debitis nulla mollitia vero voluptates possimus,
-            sequi facilis.
-          </p>
-        }
-        icon
-      />
-    </div>
+    <main className='flex min-h-screen flex-col md:flex-row'>
+      <Sidebar urls={urls} />
+
+      <div>
+        <GeneralGridLayout>
+          <div className='col-span-2 bg-neutral-400'>hi</div>
+        </GeneralGridLayout>
+        <div className='h-screen'></div>
+      </div>
+    </main>
   )
 }
 
