7e91feb7aff0cfce242fdd308e178f1fc6cd2f48
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-10-26 16:08:49 +0200

    made component for FormContent

--- app/consent-form/_components/form/FormChapter.tsx
+++ app/consent-form/_components/form/FormChapter.tsx
@@ -1,20 +1,11 @@
-type FormChapterProps = {
-  title: string
-  content: string[]
-}
+import FormContent from '@/app/consent-form/_components/form/FormContent'
+import type { FormChapterProps } from '@/utils/types'
 
 const FormChapter = ({ content, title }: FormChapterProps) => {
   return (
-    <div className='mb-12 mt-8 max-w-3xl'>
-      <h2 className='text-xl font-bold md:text-2xl'>{title}</h2>
-      <div className='mt-6 space-y-8'>
-        {content.map((text) => (
-          <p className='leading-8' key={text}>
-            {text}
-          </p>
-        ))}
-      </div>
-    </div>
+    <>
+      <FormContent content={content} title={title} />
+    </>
   )
 }
 
---
+++ app/consent-form/_components/form/FormContent.tsx
@@ -0,0 +1,18 @@
+import type { FormChapterProps } from '@/utils/types'
+
+const FormContent = ({ content, title }: FormChapterProps) => {
+  return (
+    <div className='mb-12 mt-8 max-w-3xl'>
+      <h2 className='text-xl font-bold md:text-2xl'>{title}</h2>
+      <div className='mt-6 space-y-8'>
+        {content.map((text) => (
+          <p className='leading-8' key={text}>
+            {text}
+          </p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default FormContent
