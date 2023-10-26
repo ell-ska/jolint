608d616948f02ee21f405f08d9708684a3a30ca2
robonexx <robertwagar@gmail.com>
2023-10-26 14:35:51 +0200

    ...

---
+++ app/(info-page)/_components/OurMissionSection.tsx
@@ -0,0 +1,23 @@
+const OurMissionSection = () => {
+  return (
+    <section className='flex w-full flex-col pt-12 md:pt-16'>
+      <h3 className='text-neutural-900 w-full pb-8 font-heading text-2xl font-bold md:pb-4'>
+        Our Mission
+      </h3>
+      <p className='text-neutural-900 font-body text-base leading-loose md:max-w-4xl'>
+        At Jolint, we are driven by a passion for <b>improving workplaces</b>{' '}
+        and creating a better world for everyone. We make sure to keep ethics
+        and privacy at the forefront when gathering data. Throughout our
+        journey, we&apos;ve stayed committed to these principles, ensuring that
+        {' '}<b>individuals&apos; information is handled with care and respect</b>.
+        Discover the journey of Jolint, and join us in our quest to make the
+        world a better place, one company at a time. With our innovative
+        approach, cutting-edge technologies, and a team of experts, we are
+        committed to creating a{' '}
+        <b>more inclusive and equitable work culture for all</b>.
+      </p>
+    </section>
+  )
+}
+
+export default OurMissionSection
