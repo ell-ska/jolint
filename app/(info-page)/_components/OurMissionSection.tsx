d826564eb289c02ce2578745b34f3a731712a3cc
robonexx <robertwagar@gmail.com>
2023-10-26 13:53:45 +0200

    fixed typo and /space/

--- app/(info-page)/_components/OurMissionSection.tsx
+++ app/(info-page)/_components/OurMissionSection.tsx
@@ -9,12 +9,12 @@ const OurMissionSection = () => {
         and creating a better world for everyone. We make sure to keep ethics
         and privacy at the forefront when gathering data. Throughout our
         journey, we&apos;ve stayed committed to these principles, ensuring that
-        <b> individuals&apos; information is handled with care and respect</b>.
+        {' '}<b>individuals&apos; information is handled with care and respect</b>.
         Discover the journey of Jolint, and join us in our quest to make the
         world a better place, one company at a time. With our innovative
         approach, cutting-edge technologies, and a team of experts, we are
         committed to creating a{' '}
-        <b>ore inclusive and equitable work culture for all</b>.
+        <b>more inclusive and equitable work culture for all</b>.
       </p>
     </section>
   )
