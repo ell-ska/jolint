8abbddad39c204699ec6a87650b4aa80d961c15e
idamakela <idamakela98@gmail.com>
2023-10-26 15:41:23 +0200

    re-style md sidebar on dashbord, work in progress for mobile

--- app/dashboard/_components/Sidebar.tsx
+++ app/dashboard/_components/Sidebar.tsx
@@ -20,40 +20,51 @@ const Sidebar = ({ urls }: SidebarProps) => {
     setOpen(!open)
   }
 
+  /**
+   * on Mobile: if user scrolls & nav is open -> they only see the opened side bar
+   */
+
   return (
     <>
-      <button
-        onClick={() => handleClick()}
-        className='absolute right-8 top-8 z-20 rounded-full bg-neutral-900 p-4 md:hidden'
-      >
-        {!open ? <Menu color='#FFFFFF' /> : <X color='#FFFFFF' />}
-      </button>
+      <header className='h-[56px] bg-neutral-900 md:hidden'>
+        <button
+          onClick={() => handleClick()}
+          className='absolute right-4 top-4 z-20 md:hidden'
+        >
+          {!open ? <Menu color='#FFFFFF' /> : <X color='#FFFFFF' />}
+        </button>
+      </header>
+
       <aside
         className={cn(
-          'absolute top-0 z-10 flex min-h-screen w-full flex-col items-center justify-center gap-y-28 bg-neutral-900 px-8 py-20 capitalize text-neutral-100 md:relative md:left-0 md:flex md:max-w-[14rem]',
+          'absolute top-0 z-10 flex min-h-screen w-full items-center justify-center bg-neutral-900 px-8 py-20 capitalize text-neutral-100 md:relative md:flex md:min-h-full md:max-w-[14rem]',
           !open && 'hidden',
         )}
       >
-        <div className='flex flex-col items-center gap-y-4'>
-          <UserCircle2 size={64} />
-          <h3 className='text-center font-bold'>Welcome, Anna</h3>
-          <h4>Team HR</h4>
+        <div className=' flex flex-col items-center justify-center gap-y-28 md:fixed md:top-0 md:h-screen md:justify-start md:pt-20'>
+          <div className='flex flex-col items-center gap-y-4'>
+            <UserCircle2 size={64} />
+            <h3 className='text-center font-bold'>Welcome, Anna</h3>
+            <h4>Team HR</h4>
+          </div>
+
+          <nav className='flex flex-col justify-center gap-6 text-neutral-600'>
+            {urls.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className={cn(
+                  'flex items-center gap-2',
+                  pathname === item.href && 'font-bold text-neutral-100',
+                )}
+                onClick={() => setOpen(false)}
+              >
+                {item.icon}
+                <p>{item.name}</p>
+              </Link>
+            ))}
+          </nav>
         </div>
-        <nav className='flex flex-col justify-center gap-6 text-neutral-600'>
-          {urls.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className={cn(
-                'flex items-center gap-2',
-                pathname === item.href && 'font-bold text-neutral-100',
-              )}
-            >
-              {item.icon}
-              <p>{item.name}</p>
-            </Link>
-          ))}
-        </nav>
       </aside>
     </>
   )
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,10 @@ import GeneralGridLayout from '@/app/dashboard/_components/GeneralGridLayout'
 const Dashboard = () => {
   return (
     <div>
-      <GeneralGridLayout></GeneralGridLayout>
+      <GeneralGridLayout>
+        <div className='bg-neutral-400 col-span-2'>hi</div>
+      </GeneralGridLayout>
+      <div className='h-screen'></div>
     </div>
   )
 }
