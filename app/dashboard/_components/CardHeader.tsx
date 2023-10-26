b23cccf0d4222f64726897909cc586aea590ac54
idamakela <idamakela98@gmail.com>
2023-10-26 12:10:34 +0200

    work in progress

--- app/dashboard/_components/CardHeader.tsx
+++ app/dashboard/_components/CardHeader.tsx
@@ -1,3 +1,29 @@
+'use client'
+
+import Dropdown from './Dropdown'
+
+// TESTING CODE
+//-------------------
+const dropDownTestingProps: DroptType = {
+  options: ['one', 'two', 'three'],
+  onSelect: () => onSelectTestFunction,
+}
+
+const onSelectTestFunction = (option: string) => {
+  console.log({ option })
+}
+
+type DroptType = {
+  options: string[]
+  onSelect: (value: string) => void
+}
+//-------------------
+
+type CardHeaderProps = {
+  title: string
+  currentData?: any
+}
+
 const CardHeader = () => {
   return <div></div>
 }
