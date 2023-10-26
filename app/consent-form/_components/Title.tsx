524bc6c925eca55f6f7700349bb64d3316147027
Peter Sederblad <petersederblad@Peters-MacBook-Air.local>
2023-10-26 13:39:21 +0200

    remove padding top add margin bottom

--- app/consent-form/_components/Title.tsx
+++ app/consent-form/_components/Title.tsx
@@ -5,7 +5,7 @@ type TitleProps = {
 const Title = ({ text = 'Consent Form for Data Processing' }: TitleProps) => {
   return (
     <div>
-      <h1 className='pt-12 font-heading text-2xl font-bold text-neutral-900 md:text-4xl'>
+      <h1 className='mb-4 font-heading text-2xl font-bold text-neutral-900 md:text-4xl'>
         {text}
       </h1>
     </div>
