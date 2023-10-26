79f9b812e08e0513c4d34493da22a3c31992bcad
idamakela <idamakela98@gmail.com>
2023-10-26 16:00:15 +0200

    removed testing code in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,10 +3,7 @@ import GeneralGridLayout from '@/app/dashboard/_components/GeneralGridLayout'
 const Dashboard = () => {
   return (
     <div>
-      <GeneralGridLayout>
-        <div className='bg-neutral-400 col-span-2'>hi</div>
-      </GeneralGridLayout>
-      <div className='h-screen'></div>
+      <GeneralGridLayout></GeneralGridLayout>
     </div>
   )
 }
