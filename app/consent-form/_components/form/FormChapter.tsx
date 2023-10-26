c508f7337eb2f7ddfff1a7b2535295d9d65befef
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-10-26 15:55:11 +0200

    add title and text for chapters in consent form

--- app/consent-form/_components/form/FormChapter.tsx
+++ app/consent-form/_components/form/FormChapter.tsx
@@ -4,7 +4,18 @@ type FormChapterProps = {
 }
 
 const FormChapter = ({ content, title }: FormChapterProps) => {
-  return <div>{title}</div>
+  return (
+    <div className='mb-12 mt-8 max-w-3xl'>
+      <h2 className='text-xl font-bold md:text-2xl'>{title}</h2>
+      <div className='mt-6 space-y-8'>
+        {content.map((text) => (
+          <p className='leading-8' key={text}>
+            {text}
+          </p>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default FormChapter
