094fe93b60c0d01ddf499428560f9d650b46792c
ell-ska <ella@skagerstrom.se>
2023-10-27 13:18:24 +0200

    pagination indicator for subchapters

--- app/consent-form/_components/form/FormChapter.tsx
+++ app/consent-form/_components/form/FormChapter.tsx
@@ -1,10 +1,17 @@
+import FormIndicator from '@/app/consent-form/_components/form/FormIndicator'
+
 type FormChapterProps = {
   title: string
   content: string[]
 }
 
 const FormChapter = ({ content, title }: FormChapterProps) => {
-  return <div>{title}</div>
+  return (
+    <div>
+      <FormIndicator subchapterTitle={title} />
+      {title}
+    </div>
+  )
 }
 
 export default FormChapter
