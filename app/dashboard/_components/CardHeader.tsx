17858c7bf70eb798400565f32294cf6672af6f08
idamakela <idamakela98@gmail.com>
2023-10-27 15:27:39 +0200

    re-does styling for breakpoints

--- app/dashboard/_components/CardHeader.tsx
+++ app/dashboard/_components/CardHeader.tsx
@@ -12,9 +12,9 @@ const CardHeader = ({
   selected,
 }: CardHeaderProps) => {
   return (
-    <div className='flex flex-col items-start gap-2 lg:flex-row lg:flex-wrap lg:items-center lg:justify-between'>
+    <div className='flex flex-col items-start gap-2 md:flex-row md:flex-wrap lg:items-center md:justify-between'>
       <h3 className='font-heading text-xl font-bold capitalize'>{title}</h3>
-      <div className='flex flex-col gap-2 lg:flex-row'>
+      <div className='flex flex-col gap-2 md:flex-row'>
         <div className='flex gap-2'>
           {currentData?.map(({ metric, circleColor }) => (
             <div key={metric} className='flex items-center gap-1'>
@@ -30,3 +30,7 @@ const CardHeader = ({
 }
 
 export default CardHeader
+
+
+//TODO
+// breakpoint styling 
