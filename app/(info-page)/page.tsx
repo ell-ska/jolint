33943504e45c1769b80181873d0885aa065b2315
Peter <Peter.sedberg@gmail.com>
2023-10-27 10:07:09 +0200

    first commit in new branch

--- app/(info-page)/page.tsx
+++ app/(info-page)/page.tsx
@@ -1,10 +1,13 @@
 import Hero from '@/app/(info-page)/_components/Hero'
 import Advantages from '@/app/(info-page)/_components/Advantages'
 
+import InclusionSection from '@/app/(info-page)/_components/InclusionSection'
+
 const Home = () => {
   return (
     <>
       <Hero />
+      <InclusionSection />
       <Advantages />
     </>
   )
