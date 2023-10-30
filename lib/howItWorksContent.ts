b06199f1be0221690df7bf0b2cd24d3215feed5d
ell-ska <ella@skagerstrom.se>
2023-10-30 10:14:01 +0100

    add content

---
+++ lib/howItWorksContent.ts
@@ -0,0 +1,29 @@
+const howItWorksContent = [
+  {
+    title: 'Data Extraction',
+    text: "Jolint pulls out interaction info from common work tools like email, but without touching any personal details. Imagine it like seeing where conversations happen without knowing who said what. This way, Jolint gets a bird's-eye view of how people connect at work while keeping everyone's privacy intact.",
+    image: '/how-it-works/data-extraction.svg',
+  },
+  {
+    title: 'Anonymization',
+    text: "Jolint gives everyone an anonymized ID, replacing real names and emails to keep things private. So, even when looking at how people interact, there's no way to tell who's who, ensuring everyone's privacy while still understanding the big picture of connections at work.",
+    image: '/how-it-works/anonymization',
+  },
+  {
+    title: 'Data Processing',
+    text: 'Data is transformed into a network representation where each employee is a node and interactions form connections. Each person becomes a point on the web, and the lines between them show conversations. This setup helps Jolint see how well everyone is connected, without knowing any personal details.',
+    image: '/how-it-works/data-processing',
+  },
+  {
+    title: 'AI Analysis',
+    text: 'AI and machine learning analyze data to pinpoint drivers of inclusion and belonging. They seek patterns to address questions such as new hire integration speed or how to elevate inclusion scores by addressing organizational weak points. The analysis also delves into social capital variations based on gender, role, and hire timing..',
+    image: '/how-it-works/ai-analysis',
+  },
+  {
+    title: 'Insights Interface',
+    text: "Insights from the data analysis are showcased in an intuitive interface, offering aggregated-level insights for easier understanding and action. It highlights key drivers of inclusion and recommends activities tailored to your company's specific challenges, aiding in informed decision-making.",
+    image: '/how-it-works/insights-interface',
+  },
+]
+
+export { howItWorksContent }
