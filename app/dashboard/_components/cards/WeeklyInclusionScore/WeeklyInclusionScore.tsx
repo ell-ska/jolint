191db7a01eeb37d216ac398d350432488974a9ef
ell-ska <ella@skagerstrom.se>
2023-10-31 11:20:07 +0100

    add made up data and prepare for real data

--- app/dashboard/_components/cards/WeeklyInclusionScore/WeeklyInclusionScore.tsx
+++ app/dashboard/_components/cards/WeeklyInclusionScore/WeeklyInclusionScore.tsx
@@ -7,7 +7,95 @@ import Card from '@/components/Card'
 import Dropdown from '@/app/dashboard/_components/Dropdown'
 import WeeklyInclusionScoreItem from '@/app/dashboard/_components/cards/WeeklyInclusionScore/WeeklyInclusionScoreItem'
 
-const isoWeeks = ['2022-W45', '2022-W46', '2022-W47']
+const fakeData = [
+  {
+    week: '2023-W44',
+    data: [
+      {
+        name: 'Team interactions',
+        score: 38,
+        trend: -2,
+        benchmark: 50,
+      },
+      {
+        name: 'Cross-functional interaction',
+        score: 94,
+        trend: +4,
+        benchmark: 50,
+      },
+      {
+        name: 'Informal influence',
+        score: 59,
+        trend: 0,
+        benchmark: 50,
+      },
+      {
+        name: 'Work habits',
+        score: 68,
+        trend: +1,
+        benchmark: 50,
+      },
+    ],
+  },
+  {
+    week: '2023-W43',
+    data: [
+      {
+        name: 'Team interactions',
+        score: 40,
+        trend: +2,
+        benchmark: 50,
+      },
+      {
+        name: 'Cross-functional interaction',
+        score: 90,
+        trend: +3,
+        benchmark: 50,
+      },
+      {
+        name: 'Informal influence',
+        score: 59,
+        trend: -4,
+        benchmark: 50,
+      },
+      {
+        name: 'Work habits',
+        score: 67,
+        trend: +9,
+        benchmark: 50,
+      },
+    ],
+  },
+  {
+    week: '2023-W42',
+    data: [
+      {
+        name: 'Team interactions',
+        score: 38,
+        trend: -12,
+        benchmark: 50,
+      },
+      {
+        name: 'Cross-functional interaction',
+        score: 87,
+        trend: +3,
+        benchmark: 50,
+      },
+      {
+        name: 'Informal influence',
+        score: 63,
+        trend: -2,
+        benchmark: 50,
+      },
+      {
+        name: 'Work habits',
+        score: 58,
+        trend: +5,
+        benchmark: 50,
+      },
+    ],
+  },
+]
 
 type WeeklyInclusionScoreProps = {
   title?: string
@@ -16,7 +104,8 @@ type WeeklyInclusionScoreProps = {
 const WeeklyInclusionScore = ({
   title = 'Weekly inclusion score',
 }: WeeklyInclusionScoreProps) => {
-  const [week, setWeek] = useState(formatWeek(isoWeeks[0]))
+  const [week, setWeek] = useState(fakeData[0].week)
+  const currentWeekData = fakeData.find((data) => data.week === week)
 
   return (
     <Card classname='col-span-full lg:col-span-2 h-min'>
@@ -25,23 +114,21 @@ const WeeklyInclusionScore = ({
         <Dropdown
           onSelect={(value) => setWeek(value)}
           selected={week}
-          options={isoWeeks.map((week) => formatWeek(week))}
+          options={fakeData.map((item) => item.week)}
+          formater={formatWeek}
           align='start'
         />
       </div>
       <div className='mt-6 flex flex-col gap-4 text-xs'>
-        <WeeklyInclusionScoreItem
-          name='Team interactions'
-          score={38}
-          trend={-12}
-          benchmark={50}
-        />
-        <WeeklyInclusionScoreItem
-          name='Cross-functional interaction'
-          score={87}
-          trend={+3}
-          benchmark={50}
-        />
+        {currentWeekData?.data.map(({ name, score, trend, benchmark }) => (
+          <WeeklyInclusionScoreItem
+            key={week + name}
+            name={name}
+            score={score}
+            trend={trend}
+            benchmark={benchmark}
+          />
+        ))}
       </div>
     </Card>
   )
