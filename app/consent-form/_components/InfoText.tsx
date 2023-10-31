60524578afddce2001acb7805461e9eb82ce89cd
idamakela <idamakela98@gmail.com>
2023-10-31 12:51:00 +0100

    creates InfoText file and content

---
+++ app/consent-form/_components/InfoText.tsx
@@ -0,0 +1,8 @@
+const InfoTextContent: string =
+  'Empower your workplace by authorizing consent for reliable data to enhance inclusion and belonging. Click on the steps below to read and access the consent form.'
+
+const InfoText = () => {
+  return <p>{InfoTextContent}</p>
+}
+
+export default InfoText
