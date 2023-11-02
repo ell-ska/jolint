6332ccb333b955f2be05150f6f65bac07528731d
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-11-02 11:28:46 +0100

    add trends section

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,13 @@
 
 import GeneralGridLayout from '@/app/dashboard/_components/GeneralGridLayout'
 import WeeklyInclusionScore from '@/app/dashboard/_components/cards/WeeklyInclusionScore/WeeklyInclusionScore'
+import Trends from '@/app/dashboard/_components/cards/Trends'
 
 const Dashboard = () => {
   return (
     <GeneralGridLayout>
       <WeeklyInclusionScore />
+      <Trends />
     </GeneralGridLayout>
   )
 }
