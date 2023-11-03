2dc6cae645e76272b22f6e0166842f785af1fb94
ell-ska <ella@skagerstrom.se>
2023-11-03 11:49:35 +0100

    add error toaster

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,14 +3,18 @@ import GeneralGridLayout from '@/app/dashboard/_components/GeneralGridLayout'
 import WeeklyInclusionScore from '@/app/dashboard/_components/cards/WeeklyInclusionScore/WeeklyInclusionScore'
 import Trends from '@/app/dashboard/_components/cards/Trends'
 import InclusionScore from '@/app/dashboard/_components/cards/InclusionScore/InclusionScore'
+import Toaster from '@/components/Toaster'
 
 const Dashboard = () => {
   return (
-    <GeneralGridLayout>
-      <InclusionScore />
-      <WeeklyInclusionScore />
-      <Trends />
-    </GeneralGridLayout>
+    <>
+      <Toaster />
+      <GeneralGridLayout>
+        <InclusionScore />
+        <WeeklyInclusionScore />
+        <Trends />
+      </GeneralGridLayout>
+    </>
   )
 }
 
--- hooks/useData.ts
+++ hooks/useData.ts
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react'
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosResponse, isAxiosError } from 'axios'
+
+import { useToaster } from '@/hooks/useToaster'
 
 const baseUrl =
   'https://lively-meadow-0dee68d03.4.azurestaticapps.net/data-api/rest/'
 
 const useData = (endpoint: string) => {
   const [data, setData] = useState<AxiosResponse>()
+  const { setToaster, setMessage } = useToaster((state) => ({
+    setToaster: state.setToaster,
+    setMessage: state.setMessage,
+  }))
 
   useEffect(() => {
     const fetcher = async () => {
@@ -13,12 +19,17 @@ const useData = (endpoint: string) => {
         const response = await axios.get(baseUrl + endpoint)
         setData(response)
       } catch (error) {
+        if (isAxiosError(error)) {
+          setToaster(true)
+          setMessage(error.message)
+        }
+
         console.log(error)
       }
     }
 
     fetcher()
-  }, [endpoint])
+  }, [endpoint, setMessage, setToaster])
 
   return data
 }
