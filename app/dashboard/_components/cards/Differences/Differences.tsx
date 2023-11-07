ea80918c1b6781a2179bad3243d571ec983b3491
idaakermark <ida.akermark@edu.cmeducations.se>
2023-11-07 11:06:54 +0100

    work in progress

---
+++ app/dashboard/_components/cards/Differences/Differences.tsx
@@ -0,0 +1,16 @@
+import Card from '@/components/Card'
+import CardHeader from '@/app/dashboard/_components/CardHeader'
+
+const Differences = () => {
+    return (
+        <Card classname=''>
+             {/* <CardHeader 
+                title=''
+                currentMetrics={}
+                dropdown={} 
+            />  */}
+        </Card>
+    )
+}
+
+export default Differences 
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,13 @@
 
 import GeneralGridLayout from '@/app/dashboard/_components/GeneralGridLayout'
 import WeeklyInclusionScore from '@/app/dashboard/_components/cards/WeeklyInclusionScore/WeeklyInclusionScore'
+import Differences from '@/app/dashboard/_components/cards/Differences/Differences'
 
 const Dashboard = () => {
   return (
     <GeneralGridLayout>
       <WeeklyInclusionScore />
+      <Differences />
     </GeneralGridLayout>
   )
 }
