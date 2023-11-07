f21b32d663a78eeca23b6e3d32c142090d60ee9f
Peter <Peter.sedberg@gmail.com>
2023-11-07 13:36:48 +0100

    deploy fix error fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,7 @@ import GeneralGridLayout from '@/app/dashboard/_components/GeneralGridLayout'
 import WeeklyInclusionScore from '@/app/dashboard/_components/cards/WeeklyInclusionScore/WeeklyInclusionScore'
 import Trends from '@/app/dashboard/_components/cards/Trends'
 import InclusionScore from '@/app/dashboard/_components/cards/InclusionScore/InclusionScore'
-
-import EntireSection from '@/app/dashboard/_components/RiskAndOpps/EntireSection'
+import RiskAndOppsSection from '@/app/dashboard/_components/RiskAndOpps/RiskAndOpportunitiesSection'
 
 const Dashboard = () => {
   return (
@@ -12,7 +11,7 @@ const Dashboard = () => {
       <InclusionScore />
       <WeeklyInclusionScore />
       <Trends />
-      <EntireSection />
+      <RiskAndOppsSection />
     </GeneralGridLayout>
   )
 }
