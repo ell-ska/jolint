fc51c2cebb0c558a9e186a0513acade8a90f41c8
Peter <Peter.sedberg@gmail.com>
2023-11-07 13:13:26 +0100

    change page.tsx

--- app/dashboard/_components/RiskAndOpps/RiskAndOpportunitiesSection.tsx
+++ app/dashboard/_components/RiskAndOpps/RiskAndOpportunitiesSection.tsx
@@ -3,7 +3,7 @@ import Section from '@/app/dashboard/_components/RiskAndOpps/Section'
 import { risksData } from '@/app/dashboard/_components/RiskAndOpps/data/RisksData'
 import { opportunitiesData } from '@/app/dashboard/_components/RiskAndOpps/data/OpportunitiesData'
 
-const EntireSection = () => {
+const RiskAndOppsSection = () => {
   return (
     <div className='col-span-6 md:col-span-6 lg:col-span-4'>
       <Section title='Risks' items={risksData} />
@@ -14,4 +14,4 @@ const EntireSection = () => {
   )
 }
 
-export default EntireSection
+export default RiskAndOppsSection
