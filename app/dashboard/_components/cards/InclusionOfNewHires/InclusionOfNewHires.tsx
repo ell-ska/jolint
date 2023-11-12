e81b9757b3f75d668b4bda41320ed13f4b8c2e42
idamakela <idamakela98@gmail.com>
2023-11-12 12:53:46 +0100

    removed ref

--- app/dashboard/_components/cards/InclusionOfNewHires/InclusionOfNewHires.tsx
+++ app/dashboard/_components/cards/InclusionOfNewHires/InclusionOfNewHires.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import { useData } from '@/hooks/useData'
 import { getUnique } from '@/utils/getUnique'
@@ -12,9 +12,6 @@ import Loading from '@/app/dashboard/_components/Loading'
 import type { metrics } from '@/utils/types'
 
 const InclusionOfNewHires = () => {
-  // const container = useRef<HTMLDivElement | null>(null)
-  // const [height, setHeight] = useState(0)
-
   const { data: initial, error, isLoading } = useData('demographic-timeline')
   const [currentDemographic, setCurrentDemographic] = useState<string | null>(
     null,
@@ -45,31 +42,8 @@ const InclusionOfNewHires = () => {
     update()
   }, [initial, isLoading, error, update])
 
-  /**
-   * find ref child - aka the graph
-   * get the childs height
-   */
-
-  // useEffect(() => {
-  //   if (container.current) {
-  //     let div = container.current
-  //     let children = Array.from(container.current.children)
-
-  //     if (children.length > 0) {
-  //       let child = children[0].clientHeight
-  //       console.log({ child })
-  //     }
-
-  //     console.log({ div })
-  //     console.log(children)
-  //   }
-
-  //   // return () => console.log('no child found')
-  // }, [isLoading])
-
   return (
     <Card
-      // ref={container}
       classname='col-span-full lg:col-span-3 flex flex-col gap-8'
     >
       {isLoading && <Loading />}
